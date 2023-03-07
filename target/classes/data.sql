5232cd74b7c0b4cc20ae43543b2084600cad4708
vaishnavit828 <vaishnavit828@gmail.com>
2023-03-07 05:12:56 +0000

    solution

---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO playlist (songName, lyricist, singer, musicDirector) VALUES ('Butta Bomma', 'Ramajogayya Sastry', 'Armaan Malik', 'Thaman S');
+INSERT INTO playlist (songName, lyricist, singer, musicDirector) VALUES ('Kathari Poovazhagi', 'Vijay', 'Benny Dayal, Swetha Mohan', 'A.R. Rahman');
+INSERT INTO playlist (songName, lyricist, singer, musicDirector) VALUES ('Tum Hi Ho', 'Mithoon', 'Arijit Singh', 'Mithoon');
+INSERT INTO playlist (songName, lyricist, singer, musicDirector) VALUES ('Vizhiyil', 'Vairamuthu', 'Unni Menon', 'A.R. Rahman');
+INSERT INTO playlist (songName, lyricist, singer, musicDirector) VALUES ('Nenjame', 'Panchu Arunachalam', 'S.P.Balasubrahmanyam', 'Ilaiyaraaja');
+
+
+
